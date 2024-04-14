364fa8a3a77e5d59b9d78a3c90e6e09886042524
Francischiputa02 <francisc@paylinkzm.com>
2024-04-14 11:33:35 +0200

    Changed user to clientprofile in loan model

--- loanlink/loan/urls.py
+++ loanlink/loan/urls.py
@@ -10,16 +10,16 @@ urlpatterns = [
     path('loans/active/', ActiveLoanListViewset.as_view({'get': 'list'})),
     path('loans/<str:loan_id>', ActiveLoanListViewset.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy' })),
 
-    path('loans/pending', PendingLoanListViewset.as_view({'get': 'list'})),
+    path('loans/pending/', PendingLoanListViewset.as_view({'get': 'list'})),
     path('loans/<str:loan_id>', PendingLoanListViewset.as_view({'get': 'retrieve', 'put': 'update','delete': 'destroy'})),
 
-    path('loans/closed', ClosedLoanListViewset.as_view({'get': 'list'}), name='closeloans'),
+    path('loans/closed/', ClosedLoanListViewset.as_view({'get': 'list'}), name='closeloans'),
     path('loans/<str:loan_id>', ClosedLoanListViewset.as_view({'get': 'retreive', 'put': 'update', 'delete': 'destroy'}), name='closeloans'),
 
-    path('loans/rejected', RejectedLoanListViewest.as_view({'get': 'list'}), name='rejectedloan'),
+    path('loans/rejected/', RejectedLoanListViewest.as_view({'get': 'list'}), name='rejectedloan'),
     path('loans/<str:loan_id>', RejectedLoanListViewest.as_view({'get': 'retreive', 'put': 'update', 'delete': 'destroy'}), name='rejectedloansid'),
 
-    path('loans/approved', ApprovedLoanListView.as_view({'get': 'list'}), name='approvedloans'),
+    path('loans/approved/', ApprovedLoanListView.as_view({'get': 'list'}), name='approvedloans'),
     path('loans/<str:loan_id>', ApprovedLoanListView.as_view({'get': 'retreive', 'put': 'update', 'delete': 'destroy'}), name='approvedloansid'),
 
     path('creditscore/', CreditScoreSerializerViewset.as_view({'get': 'list', 'post': 'create'}), name='creditscore'),
